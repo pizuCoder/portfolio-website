c092d220c9643251420664830f155fd028cb27fb
Priya Moghe <102720207+pizuCoder@users.noreply.github.com>
2023-02-26 19:14:51 +0530

    dynamic background added

---
+++ src/components/HomePage/Background.js
@@ -0,0 +1,42 @@
+import './Background.css'
+
+export default function Background() {
+    return(
+        <div class="background">
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+</div>
+    )
+}
